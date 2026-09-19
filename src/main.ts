d6f8888244521a6e7543d30f0a184b429bf15187
abi15punkte <abi.15.punkte@gmail.com>
2026-09-19 14:02:32 +0200

    Unnötige Gitter-URL-Logik entfernen

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,1 @@
 import "./style.css";
-
-const gridBackgroundUrl = new URL(
-  `Gitter.png`,
-  new URL(import.meta.env.BASE_URL, window.location.href)
-).href;
-
-document.documentElement.style.setProperty(
-  "--grid-background",
-  `url("${gridBackgroundUrl}")`
-);
